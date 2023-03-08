f7cda82a029293811d018504ad9f2684fb452fb6
Tayyab Sajjad <devtayyabsajjad@gmail.com>
2023-03-09 01:06:32 +0500

    First Version

---
+++ java.js
@@ -0,0 +1,134 @@
+//1 Declare two variables: admin and name. Assign the value "Umair Azmat" to name. Copy the value 
+// from name to admin. Show the value of admin using alert (must output Name)?
+
+// var name;
+// name = "Umair Azmat";
+// var admin;
+// admin = name;
+// alert(admin);
+
+// 2 Create a variable with the name of our planet. How would you name such a variable?
+
+// var nameOfourplanet;
+
+// 3 Create a variable to store the name of a current visitor to a website. How would you name that variable?
+
+// var currentVisitor
+
+// 4 Create a string variable called favDrink and assign a value to it?
+
+// var favDrink = "pepsi";
+// console.log(favDrink);
+
+// 5 Create a number variable called favNum and assign a value to it?
+// var num = 10;
+// console.log(num);
+
+// 6 Create a function block called myFavs that console logs your answers,
+//  don't forget to invoke the myFavs function?
+
+// function myFav(){
+//     console.log("My favourite color is blue.")
+//     console.log("My favourite food is vegetables.")
+//     console.log("My favourite movie is G-force.")
+// }
+// myFav();
+
+// 7 Create an object called person, with name set to Umair and age set to 20?
+
+// let person = {
+//     name:"umair",
+//     age: 22
+// }
+// console.log("Name:", person.name);
+// console.log("Age:", person.age);
+
+// 8 write a JS program, define two variables and sum those two variable and 
+// save result in third variable and print in console?
+
+// var firstNum = 2;
+// var secondNum = 4;
+// var sum = firstNum + secondNum ;
+// console.log(sum);
+
+// 9 write a JS program, that can divide two numbers and show the result in 
+// console? console.log("result of firstProgram", a);
+
+// var firstNum = 4;
+// var secondNum = 2;
+// var ans = firstNum/secondNum;
+// console.log("answer is " ,ans);
+
+// 10 	write a JS program, that can add two number, divide result by 5 and show in 
+// console?console.log("result of firstProgram", a);
+
+// function calculate(a,b){
+//     return (a + b)/5;
+// }
+// var firstNum = 10;
+// var secondNum = 5;
+// var ans = calculate(firstNum,secondNum);
+// console.log("answer is ",ans);
+
+// 11 
+// 11.	write a JS program, that can convert temp from F into C and show into console, 
+// using C = (F − 32) * 5/9. use if F = 50,  what will be the value of C? console.log("Divide two number", a)?
+
+// function temp(F){
+//     return (F - 32) * 5/9;
+// }
+// F = 50;
+//  var a = temp(F);
+// console.log("Temperature is ",a);
+
+// // 12 Write a JavaScript program to find the sum of two numbers obtained from prompt and
+// display the result using alert.?
+// var inputNum1 = prompt("Enter First Num");
+// var inputNum2 = prompt("Enter second Num");
+// alert(sum ="Sum is " + add(inputNum1,inputNum2));
+
+// 13 Write a JavaScript program to find the product of two numbers obtained from prompt and display the
+//  result using alert.
+
+// function multiply(c,d){
+//     return c*d;
+// }
+// var inputNum1 = prompt("Enter First Num");
+// var inputNum2 = prompt("Enter second Num");
+// alert("Ans is " + multiply(inputNum1,inputNum2));
+
+//14 Write a JavaScript program to find the product of two numbers obtained from prompt and display 
+// the result using alert.?
+// function sub(c,d){
+//     return c-d;
+// }
+// var inputNum1 = prompt("Enter First Num");
+// var inputNum2 = prompt("Enter second Num");
+// alert("Ans is " + sub(inputNum1,inputNum2));
+
+//15 Write a JavaScript program to find the quotient of two numbers obtained from prompt and display the result
+//  using console.log.?
+// function quotient(c,d){
+//     return c/d;
+// }
+// var inputNum1 = prompt("Enter First Num");
+// var inputNum2 = prompt("Enter second Num");
+// alert("Ans is " + quotient(inputNum1,inputNum2));
+
+// 16 Write a JavaScript program to find the remainder of two numbers obtained from prompt using the modulus 
+// operator and display the result using alert?
+
+// function reminder(c,d){
+//     return c%d;
+// }
+// var inputNum1 = prompt("Enter First Num");
+// var inputNum2 = prompt("Enter second Num");
+// alert("Ans is " + reminder(inputNum1,inputNum2));
+
+
+
+
+
+
+
+
